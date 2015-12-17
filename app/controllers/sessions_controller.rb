830ba7c31b3d8bc6d0b09b54d631430d5a2d86bc
do0fusz <do0fusz@hotmail.com>
2015-12-17 19:03:54 +0100

    updated

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController 
+
+	def new
+	end
+
+	def create
+		@user = User.find_by_email(params[:email])
+		if @user && @user.authenticate(params[:password])
+			sessoin[:user_id] = @user.id 
+			redirect_to '/'
+		else
+			render :new
+		end
+	end
+
+	def destroy 
+		session[:user_id] = nil 
+		redirect_to sign_in_path
+	end
+
+end
